b99301601ad3e19623ef0f84ec397ea3c53ec2f8
igochev <redial@abv.bg>
2023-11-29 17:44:33 +0200

    redis pool

---
+++ dependencies.py
@@ -0,0 +1,14 @@
+from redis import Redis, ConnectionPool
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# Redis connection pool
+connection_pool = ConnectionPool(host=os.getenv('REDIS_HOST', 'localhost'), 
+                                 port=os.getenv('REDIS_PORT', 6379), 
+                                 db=os.getenv('REDIS_DB', 0), 
+                                 password=os.getenv('REDIS_PASSWORD', None))
+
+def get_redis() -> Redis:
+    return Redis(connection_pool=connection_pool)
